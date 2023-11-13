5cae907be0e5d42ee518c9f024784643f46cde43
SciChart DevTeam <devteam@scichart.com>
2023-11-13 13:55:51 +0100

    [Nightly] SciChart(4.5.0-nightly.5887)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let repo = "PodSpecs-Nightly"
-let version = "4.4.2-nightly.5878"
+let version = "4.5.0-nightly.5887"
 
 let package = Package(
     name: "SciChart",
@@ -21,7 +21,7 @@ let package = Package(
         .binaryTarget(
             name: "SciChart",
             url: "https://github.com/ABTSoftware/\(repo)/releases/download/v\(version)/SciChart_iOS_\(version).zip",
-            checksum: "53ecc38cb0786dbbf8da1267bed2090ac0f9d2693a3117ffcc5e3ebff67bf044"
+            checksum: "c778a11de75822c0d5e251ea97cff428fbc1c399228b178bc1c2d3697d90b542"
         ),
     ]
 )
